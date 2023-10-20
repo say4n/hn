267fc57bddaa50803469fbcf5fc1c6b067f1ecab
Sayan Goswami <goswami.sayan47@gmail.com>
2023-10-20 02:54:26 +0100

    Use CacheAPI for caching.

--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -1,27 +1,28 @@
 import { appCacheName } from "./const";
 
-const setItemWithExpiry = (key: string, value:string, ttl: number = 60) => {
-  localStorage.setItem(
+const setItemWithExpiry = async (key: string, value: string, ttl: number = 60) => {
+  const cache = await caches.open(appCacheName)
+  await cache.put(
     key,
-    JSON.stringify(
+    new Response(JSON.stringify(
       {
         expiry: new Date().getTime() + ttl * 1_000,
         payload: value,
       }
-    )
+    ))
   )
 }
 
-const getItemWithExpiry = (key: string) => {
-  const item = localStorage.getItem(key)
+const getItemWithExpiry = async (key: string) => {
+  const cache = await caches.open(appCacheName)
 
-  if (!item)
+  if (!(await cache.match(key))?.ok)
     return
 
-  const { expiry, payload } = JSON.parse(item)
+  const { expiry, payload } = await (await cache.match(key))?.json()
 
   if (new Date().getTime() > expiry) {
-    localStorage.removeItem(key)
+    cache.delete(key)
     return
   }
 
@@ -29,9 +30,8 @@ const getItemWithExpiry = (key: string) => {
 }
 
 export const fetcher = async (url: URL | string) => {
-  let mUrl = new URL(url);
-  let cacheKey = `${appCacheName}-${mUrl.pathname}`
-  let item = getItemWithExpiry(cacheKey)
+  const cacheKey = new URL(url).pathname;
+  const item = await getItemWithExpiry(cacheKey)
 
   if (item)
     return JSON.parse(item)
@@ -39,7 +39,8 @@ export const fetcher = async (url: URL | string) => {
   const response =  await fetch(url)
   const json = await response.json()
   
-  setItemWithExpiry(cacheKey, JSON.stringify(json))
-  
-  return JSON.parse(getItemWithExpiry(cacheKey)!!)
+  await setItemWithExpiry(cacheKey, JSON.stringify(json))
+  const itemAfterSet = await getItemWithExpiry(cacheKey)
+
+  return JSON.parse(itemAfterSet)
 }
